a0eab63209f3a3905bece5562f106da6a089d437
Evan Tahler <evan@evantahler.com>
2024-03-03 17:04:31 -0800

    smoke test for config

---
+++ __tests__/config.test.ts
@@ -0,0 +1,40 @@
+import {
+  test,
+  expect,
+  describe,
+  beforeAll,
+  beforeEach,
+  afterAll,
+} from "bun:test";
+
+import { config } from "../config";
+import { loadFromEnvIfSet } from "../util/config";
+
+test("config can be loaded", () => {
+  expect(config).toBeDefined();
+});
+
+test("config can have sub-parts", () => {
+  expect(typeof config.server.web.port).toBe("number");
+  expect(config.server.web.port).toEqual(8080);
+});
+
+describe("updating config", () => {
+  let originalPort: number;
+  beforeAll(() => {
+    originalPort = config.server.web.port;
+  });
+
+  beforeEach(() => {});
+
+  afterAll(() => {
+    config.server.web.port = originalPort;
+  });
+
+  test("config can be overridden by environment variables", () => {
+    expect(loadFromEnvIfSet("servers.web.port", 8080)).toEqual(8080);
+
+    Bun.env["servers.web.port"] = "8081";
+    expect(loadFromEnvIfSet("servers.web.port", 8080)).toEqual(8081);
+  });
+});
