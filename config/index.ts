af9a2854106d1e96d45543bb4e3b22a44550c04f
Evan Tahler <evan@evantahler.com>
2024-03-24 19:35:51 -0700

    env test?

--- config/index.ts
+++ config/index.ts
@@ -2,10 +2,12 @@ import { configLogger } from "./logger";
 import { configProcess } from "./process";
 import { configServerWeb } from "./server/web";
 import { configDatabase } from "./database";
+import { configRedis } from "./redis";
 
 export const config = {
   process: configProcess,
   logger: configLogger,
   database: configDatabase,
+  redis: configRedis,
   server: { web: configServerWeb },
 };
